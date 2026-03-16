e96874ec371d6e6e28620f6b2246702e995bb9b0
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-03-16 23:00:48 +0530

    part2

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard =4;
 
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let card = 7;
 
-if(sum <= 20){
-  message = "Do you want to draw a new card? 🙂";
-}
-else if(sum === 21){
-  message = "Wohoo! You've got Blackjack! 🥳"
-  hasBlackJack = true;
-}
-else{
-  message = "You're out of the game! 😭"
-  isAlive = false;
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame(){
+  renderGame();
 }
 
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+function renderGame(){
+  cardsEl.textContent = "Cards :" + firstCard +" " + secondCard;
+  sumEl.textContent = "Sum :" + sum;
+  console.log("the game is starting");
+  if(sum <= 20){
+    message = "Do you want to draw a new card? 🙂";
+  }
+  else if(sum === 21){
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true;
+  }
+  else{
+    message = "You're out of the game! 😭"
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+  console.log(hasBlackJack);
+  console.log(isAlive);
+  console.log(message);
+
+}
+
+
+function newCard(){
+  console.log("Drawing a new card from the deck");
+  sum += card;
+  renderGame();
+}
 
  
